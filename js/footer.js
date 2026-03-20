9f23772b53ded7e52cd66c5a6ea222209681e815
yperez-dev <yperez.yu@gmail.com>
2026-03-19 22:27:58 -0600

    feat: agrega pie de página compartido con JavaScript

---
+++ js/footer.js
@@ -0,0 +1,20 @@
+const footer = `
+  <footer class="footer">
+
+    <div class="footer-logo">
+      <p>El legado de H.P. Lovecraft</p>
+    </div>
+
+    <div class="footer-credits">
+      <p>Sitio creado por <span>Yubini Emanuel Pérez Oajaca</span></p>
+      <p></p>
+    </div>
+
+    <div class="footer-copy">
+      <p>© 2026 El legado de H.P. Lovecraft</p>
+    </div>
+
+  </footer>
+`;
+
+document.body.insertAdjacentHTML('beforeend', footer);
